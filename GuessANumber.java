4a4d2c454dc54e2c4a878a12c705d2ba1ab4b333
Brendan DeMilt <bdemilt98@gmail.com>
2017-01-25 13:45:30 -0500

    Setting up the java file

---
+++ GuessANumber.java
@@ -0,0 +1,25 @@
+/*
+Brendan DeMilt Chris Pan
+1/25/17
+Period: 8
+guess a Number git tested
+*/
+
+public class GuessANumber{
+
+
+
+
+
+
+
+public static void main(String[] args){
+
+System.out.println("please lord in heaven above work");
+
+
+}
+
+
+
+}
